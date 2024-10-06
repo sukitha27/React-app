336d204e400355b2832aaf847dc58860aa0db95b
sukitha27 <sukithabandara13@gmail.com>
2024-10-06 12:57:54 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import { LoginSignup } from './components/Assets/LoginSignup/LoginSignup';
 
 function App() {
   return (
-    <div className="App">
+    <div>
+      <LoginSignup/>
       
     </div>
   );
